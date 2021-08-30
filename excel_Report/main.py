812400294727156f4ec252469a8e5877ee5882cd
Salusmoon <Salusmoon@github.com>
2021-08-30 14:38:13 +0300

    Metodların düzenlenmesi

--- excel_Report/main.py
+++ excel_Report/main.py
@@ -5,6 +5,7 @@ import xlsxwriter
 import platform
 import xlrd
 
+# İşletim sistemi ve path işlemleri
 os_platform = platform.system()
 first_excel_path = sys.argv[1]
 second_excel_path= sys.argv[2]
@@ -22,46 +23,82 @@ elif os_platform == "Windows" :
 first_excel_sheet_names = xlrd.open_workbook(first_excel, on_demand=True).sheet_names()
 second_excel_sheet_names = xlrd.open_workbook(first_excel, on_demand=True).sheet_names()
 
+#  
+# excel: excel dosyası      sheetName: İstenilen excel sayfası      durum: istenilen test durumu
+# return dataFrame
+def excelRead(excel, sheetName, Testdurum):
+    df=pd.read_excel(excel, sheetName)
+    df=df1.loc[df1["Durum"] == Testdurum]
+    return df
+#  
+# data: filtrelenmiş data
+# return sadece test isimleri 
+def testCases(data):
+    testCase=[]
+    for index, row in data.iterrows():
+        testCase.append(row["Test Senaryosu"])
+    return testCase
+#  
+# data1: Birinci excelden istelilen durumun test isimleri       data2 : ikinci excelden istelilen durumun test isimleri
+# return: birinci excelde olup ikinci excelde olamayan test isimleri
+def findTestCaseDiff(data1, data2):
+    uniqueTestCases = []
+    count_dup=0
+    for i in range(len(data1)):
+        if (data1[i] in data2):
+            count_dup=count_dup+1
+        else:
+            uniqueTestCases.append(data1[i])
+    return uniqueTestCases
+#  
+# data: testScenarioResult verisi
+# return düzenlenmiş TestCloudResult tablosuna çevrilmiş Data
+def dataTestScenarioResılt(data):
+    data_test = []
+    data_durum = []
+    for index, row in data.iterrows():
+        if row["Başarılı"]== 1: 
+            data_durum.append("SUCCESS")
+            data_test.append(row["Test Senaryosu"])
+        elif row["Uyarı"]==1:
+            data_durum.append("WARNING")
+            data_test.append(row["Test Senaryosu"])
+        elif row["Başarısız"]==1:
+            data_durum.append("FAILURE")
+            data_test.append(row["Test Senaryosu"])
+        elif row["Hatalı"]==1:
+            data_durum.append("ERROR")
+            data_test.append(row["Test Senaryosu"])
+        else:
+            data_durum.append("BLOCKED")
+            data_test.append(row["Test Senaryosu"])
+    dict = {"Test Senaryosu": data_test, "Durum": data_durum}
+    newData= pd.DataFrame(data=dict)
+    return newData
+
+
 
 if ("TestiniumCLOUDResults" in first_excel_sheet_names and "TestiniumCLOUDResults" in second_excel_sheet_names):
 
-    df1 = pd.read_excel(first_excel, "TestiniumCLOUDResults")
-    df2 = pd.read_excel(second_excel, "TestiniumCLOUDResults")
+    df1 = excelRead(first_excel, "TestiniumCLOUDResults", durum)
+    df2 = excelRead(first_excel, "TestiniumCLOUDResults", durum)
 
-    df1= df1.loc[df1["Durum"] == durum]
-    df2= df2.loc[df2["Durum"] == durum]
+    df1_test = testCases(df1)
+    df2_test = testCases(df2)
 
-    passCasedf1 = []
-    df1_test=[]
-    df2_test=[]
-
-    for index, row in df1.iterrows():
-        df1_test.append(row["Test Senaryosu"])
-
-
-    for index, row in df2.iterrows():
-        df2_test.append(row["Test Senaryosu"])
-
-    count_dup=0
-    for i in range(len(df1_test)):
-        if (df1_test[i] in df2_test):
-            count_dup=count_dup+1
-        else:
-            passCasedf1.append(df1_test[i])
-
-    print("ilk testten " , len(passCasedf1), " adet farklı case geçmiş")
+    testCaseDiff= findTestCaseDiff(df1_test,df2_test)
+    
+    #print("ilk testten " , len(passCasedf1), " adet farklı case geçmiş")
 
     workbook = xlsxwriter.Workbook('result.xlsx')
     worksheet = workbook.add_worksheet()
     A="A"
     column = first_excel_path+ " de olup " + second_excel_path + "de olmayan, durumu " + durum + " olan caseler "
     worksheet.write("A1", column)
-    for i in range(len(passCasedf1)):
+    for i in range(len(testCaseDiff)):
 
         index= str(i+2)
-        worksheet.write(A+index, passCasedf1[i])
-
-
+        worksheet.write(A+index, testCaseDiff[i])
     workbook.close()
 
 else:
@@ -69,84 +106,28 @@ else:
     df1 = pd.read_excel(first_excel, "TestScenarioResults")
     df2 = pd.read_excel(second_excel, "TestScenarioResults")
 
-    df1_test = []
-    df1_durum = []
-
-    for index, row in df1.iterrows():
-        if row["Başarılı"]== 1: 
-            df1_durum.append("SUCCESS")
-            df1_test.append(row["Test Senaryosu"])
-        elif row["Uyarı"]==1:
-            df1_durum.append("WARNING")
-            df1_test.append(row["Test Senaryosu"])
-        elif row["Başarısız"]==1:
-            df1_durum.append("FAILURE")
-            df1_test.append(row["Test Senaryosu"])
-        elif row["Hatalı"]==1:
-            df1_durum.append("ERROR")
-            df1_test.append(row["Test Senaryosu"])
-        else:
-            df1_durum.append("BLOCKED")
-            df1_test.append(row["Test Senaryosu"])
-
-    dict = {"Test Senaryosu": df1_test, "Durum": df1_durum}
-    df1= pd.DataFrame(data=dict)
-
-    df2_test=[]
-    df2_durum=[]
-    for index, row in df2.iterrows():
-        if row["Başarılı"]== 1:
-            df2_durum.append("SUCCESS")
-            df2_test.append(row["Test Senaryosu"])
-        elif row["Uyarı"]==1:
-            df2_durum.append("WARNING")
-            df2_test.append(row["Test Senaryosu"])
-        elif row["Başarısız"]==1:
-            df2_durum.append("FAILURE")
-            df2_test.append(row["Test Senaryosu"])
-        elif row["Hatalı"]==1:
-            df2_durum.append("ERROR")
-            df2_test.append(row["Test Senaryosu"])
-        else:
-            df2_durum.append("BLOCKED")
-            df2_test.append(row["Test Senaryosu"])
-
-    dict = {"Test Senaryosu": df2_test, "Durum": df2_durum}
-    df2= pd.DataFrame(data=dict)
+    df1=dataTestScenarioResılt(df1)
+    df2=dataTestScenarioResılt(df2)
 
     df1= df1.loc[df1["Durum"] == durum]
     df2= df2.loc[df2["Durum"] == durum]
 
-    df1_test=[]
-    df2_test=[]
+    df1_test=testCases(df1)
+    df2_test=testCases(df2)
 
-    for index, row in df1.iterrows():
-        df1_test.append(row["Test Senaryosu"])
-    for index, row in df2.iterrows():
-        df2_test.append(row["Test Senaryosu"])
+    testCaseDiff= findTestCaseDiff(df1_test,df2_test)
 
-    passCasedf1=[]
-    count_dup=0
-    for i in range(len(df1_test)):
-        if (df1_test[i] in df2_test):
-            count_dup=count_dup+1
-        else:
-            passCasedf1.append(df1_test[i])
-
-
-    print("ilk testten " , len(passCasedf1), " adet farklı case geçmiş")
+    #print("ilk testten " , len(testCaseDiff), " adet farklı case geçmiş")
 
     workbook = xlsxwriter.Workbook('result.xlsx')
     worksheet = workbook.add_worksheet()
     A="A"
     column = first_excel_path+ " de olup " + second_excel_path + "de olmayan, durumu " + durum + " olan caseler "
     worksheet.write("A1", column)
-    for i in range(len(passCasedf1)):
+    for i in range(len(testCaseDiff)):
 
         index= str(i+2)
-        worksheet.write(A+index, passCasedf1[i])
-
-
+        worksheet.write(A+index, testCaseDiff[i])
     workbook.close()
 
 print("finish")
